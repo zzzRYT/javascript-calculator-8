0ae0eab183616bfa7c00442f5af5fab4e69f27f9
zzzryt <jinjinstar3@gmail.com>
2025-10-14 18:41:59 +0900

    feat(separator): 구분자 불러오는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import { INPUT_DESCRIPTION } from './constants.js';
+
 class App {
-  async run() {}
+  constructor() {
+    this.separator = [',', ':'];
+  }
+
+  getSeparator(input) {
+    const defaultSeparatorString = this.separator.join('|');
+    const regex = new RegExp(`[${defaultSeparatorString}]`, 'g');
+    const splitNumbers = input.split(regex);
+    return splitNumbers;
+  }
+
+  calculator(input) {
+    const splitNumbers = this.getSeparator(input);
+
+    Console.print(splitNumbers);
+  }
+
+  async run() {
+    const userInput = Console.readLineAsync(`${INPUT_DESCRIPTION}\n`);
+
+    userInput.then((input) => {
+      this.calculator(input);
+    });
+  }
 }
 
 export default App;
