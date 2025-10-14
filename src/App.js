45d4b4e2a3d763e204560dd0e1d24a28ff7945e1
zzzryt <jinjinstar3@gmail.com>
2025-10-15 01:03:47 +0900

    docs(OOP): OOP로 바라본 구현 정리

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ class App {
     this.separator = [',', ':'];
   }
 
+  // App class (input) - 값을 받아서 에러를 발생시킬지 or calculator class를 실행할지 결정
+
+  // calculator class (땨로 쪼개기?) - 그냥 계산, 중간에 validation
+
+  // App class (output) - 어떤 값이 들어오든(에러든, 정상적인 데이터든) 받아서 표출할 수 있도록 함
+
+  addCustomSeparator() {}
+
   getNumberFromInput(input) {
     const defaultSeparatorString = this.separator.join('|');
     const regex = new RegExp(`[${defaultSeparatorString}]`, 'g');
@@ -30,13 +38,10 @@ class App {
   }
 
   async run() {
-    const userInput = Console.readLineAsync(`${INPUT_DESCRIPTION}\n`);
-
     try {
-      userInput.then((input) => {
-        const answer = this.calculator(input);
-        Console.print(answer);
-      });
+      const userInput = await Console.readLineAsync(`${INPUT_DESCRIPTION}\n`);
+      const answer = this.calculator(userInput);
+      Console.print(answer);
     } catch (error) {
       Console.print(error);
     }
